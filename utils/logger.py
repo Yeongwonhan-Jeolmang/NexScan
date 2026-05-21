da7320cf6da9c0064fe4de98ecdba801d6300dad
Yeongwonhan-Jeolmang <hanaeunseoo@daum.net>
2026-05-21 16:36:29 -0400

    logger reformat

--- utils/logger.py
+++ utils/logger.py
@@ -1,12 +1,18 @@
 """NexScan - Logger utility."""
+
 import logging
 import sys
+
 
 def get_logger(name: str) -> logging.Logger:
     logger = logging.getLogger(name)
     if not logger.handlers:
         handler = logging.StreamHandler(sys.stderr)
-        handler.setFormatter(logging.Formatter("%(asctime)s [%(levelname)s] %(name)s: %(message)s",datefmt="%H:%M:%S"))
+        handler.setFormatter(
+            logging.Formatter(
+                "%(asctime)s [%(levelname)s] %(name)s: %(message)s", datefmt="%H:%M:%S"
+            )
+        )
 
         logger.addHandler(handler)
     logger.setLevel(logging.INFO)
